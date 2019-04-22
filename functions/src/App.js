e6bcb246e6d0f683e92825670a9840f98ac05c7a
KumquatFelafel <lernj88@gmail.com>
2019-04-22 11:43:58 -0400

    Fixed css for react.

--- functions/src/App.js
+++ functions/src/App.js
@@ -1,4 +1,11 @@
 import React, {Component} from 'react';
+import './resources/vendors/css/grid.css';
+import './resources/vendors/css/normalize.css';
+import './resources/vendors/fonts/_ionicons_svg_ios-bulb.svg';
+import './resources/vendors/fonts/_ionicons_svg_ios-mic.svg';
+import './resources/vendors/fonts/_ionicons_svg_ios-people.svg';
+import './resources/vendors/fonts/_ionicons_svg_ios-ribbon.svg';
+import './resources/css/style.css'
 import Navbar from "./components/Navbar";
 import Guidelines from "./components/Guidelines";
 import Teams from "./components/Teams";
--- functions/src/components/Guidelines.js
+++ functions/src/components/Guidelines.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 
+
 class Guidelines extends Component {
   render() {
     return (
@@ -12,26 +13,26 @@ class Guidelines extends Component {
           </div>
           <div className="row">
             <div className="col span-1-of-4 box">
-              <ion-icon name="people" className="icon-big"></ion-icon>
+              <ion-icon name="people" className="icon-big"/>
               <h3>Teamwork</h3>
               <p>Form a team of up to 4 members to collaborate on a joint project. You can also go solo if thats your style.
               </p>
             </div>
             <div className="col span-1-of-4 box">
-              <ion-icon name="bulb" className="icon-big"></ion-icon>
+              <ion-icon name="bulb" className="icon-big"/>
               <h3>Creativity is Key</h3>
               <p>The theme for this event is creativity in coding. This gives you wide latitude to build what motivates you.
               </p>
             </div>
             <div className="col span-1-of-4 box">
-              <ion-icon name="microphone" className="icon-big"></ion-icon>
+              <ion-icon name="microphone" className="icon-big"/>
               <h3>Presentation</h3>
               <p>You will present your project in front of a general audience. The audience will then fill out a poll about
                 your project. Your project will be judged by the CS Professors.
               </p>
             </div>
             <div className="col span-1-of-4 box">
-              <ion-icon name="ribbon" className="icon-big"></ion-icon>
+              <ion-icon name="ribbon" className="icon-big"/>
               <h3>Sweet victory</h3>
               <p>The audience will be polled on what their favorite project was and the projects judged most highly by the
                 professors will be declared winners. The winners will have the oportunity to record a video about their
