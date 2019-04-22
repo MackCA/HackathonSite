5f3c9e1cbc4d6757ad0ab3a254c082972813ed04
KumquatFelafel <lernj88@gmail.com>
2019-04-22 01:30:21 -0400

    Teams: Got rid of duplicates.

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -33,82 +33,38 @@ class Teams extends Component {
             <Team
                 imgSrc={Team2Emblem}
                 imgAlt={"Team 2 Emblem"}
-                teamName={}
+                teamName={"Biohazard"}
             />
             <Team
                 imgSrc={Team3Emblem}
                 imgAlt={"Team 3 Emblem"}
-                teamName={}
+                teamName={"The Empire"}
             />
             <Team
                 imgSrc={Team4Emblem}
                 imgAlt={"Team 4 Emblem"}
-                teamName={}
+                teamName={"The First Order"}
             />
             <Team
                 imgSrc={Team5Emblem}
                 imgAlt={"Team 5 Emblem"}
-                teamName={}
+                teamName={"The Jedi Order"}
             />
             <Team
                 imgSrc={Team6Emblem}
                 imgAlt={"Team 6 Emblem"}
-                teamName={}
+                teamName={"The Batman"}
             />
             <Team
                 imgSrc={Team7Emblem}
                 imgAlt={"Team 7 Emblem"}
-                teamName={}
+                teamName={"The Main Event"}
             />
             <Team
                 imgSrc={Team8Emblem}
                 imgAlt={"Team 8 Emblem"}
-                teamName={}
+                teamName={"The Crazy One"}
             />
-            <li>
-              <figure className="team-emblem">
-                <img src="\resources\img\download.jpg" alt="Team 2 Emblem" rel="emblem"/>
-                  <figcaption className="captions">Team: Biohazard</figcaption>
-              </figure>
-            </li>
-            <li>
-              <figure className="team-emblem">
-                <img src="\resources\img\EF-EMB-P-00053.jpg" alt="Team 3 Emblem" rel="emblem"/>
-                  <figcaption className="captions">Team: The Empire</figcaption>
-              </figure>
-            </li>
-            <li>
-              <figure className="team-emblem">
-                <img src="\resources\img\EF-EMB-P-00071.jpg" alt="Team 4 Emblem" rel="emblem"/>
-                  <figcaption className="captions">Team: The First Order</figcaption>
-              </figure>
-            </li>
-          </ul>
-          <ul className="teams-showcase">
-            <li>
-              <figure className="team-emblem">
-                <img src="\resources\img\EF-EMB-P-00074.jpg" alt="Team 5 Emblem" rel="emblem"/>
-                  <figcaption className="captions">Team: The Jedi Order</figcaption>
-              </figure>
-            </li>
-            <li>
-              <figure className="team-emblem">
-                <img src="\resources\img\batman.jpg" alt="Team 6 Emblem" rel="emblem"/>
-                  <figcaption className="captions">Team: The Batman</figcaption>
-              </figure>
-            </li>
-            <li>
-              <figure className="team-emblem">
-                <img src="\resources\img\images.png" alt="Team 7 Emblem" rel="emblem"/>
-                  <figcaption className="captions">Team: The Main Event</figcaption>
-              </figure>
-            </li>
-            <li>
-              <figure className="team-emblem">
-                <img src="\resources\img\20653.jpg" alt="Team 8 Emblem" rel="emblem"/>
-                  <figcaption className="captions">Team: The Crazy One</figcaption>
-              </figure>
-            </li>
           </ul>
         </section>
     );
