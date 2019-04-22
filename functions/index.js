c8d568978299374c7215ede81083ed945edcb4b9
KumquatFelafel <lernj88@gmail.com>
2019-04-22 07:54:49 -0400

    modified index

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,16 @@
-const functions = require('firebase-functions');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom'
+import App from '/src/App'
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-<script src="https://www.gstatic.com/firebasejs/5.10.0/firebase.js"></script>
-<script>
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCNzDj-mtM8EeSVp1oX1YTwJoih-qdUxYI",
-    authDomain: "sccc-hackathon-spring-2019.firebaseapp.com",
-    databaseURL: "https://sccc-hackathon-spring-2019.firebaseio.com",
-    projectId: "sccc-hackathon-spring-2019",
-    storageBucket: "sccc-hackathon-spring-2019.appspot.com",
-    messagingSenderId: "792531230283"
-  };
-  firebase.initializeApp(config);
-</script>
+/*</script>*/
+
+ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+//serviceWorker.unregister();
