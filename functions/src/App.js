417e30fbe7e19cd31b885e5eeb6d121832b53d3f
KumquatFelafel <lernj88@gmail.com>
2019-04-22 12:17:22 -0400

    Threw header into... well... Header.

--- functions/src/App.js
+++ functions/src/App.js
@@ -6,9 +6,9 @@ import './resources/vendors/fonts/_ionicons_svg_ios-mic.svg';
 import './resources/vendors/fonts/_ionicons_svg_ios-people.svg';
 import './resources/vendors/fonts/_ionicons_svg_ios-ribbon.svg';
 import './resources/css/style.css'
-import Navbar from "./components/Navbar";
 import Guidelines from "./components/Guidelines";
 import Teams from "./components/Teams";
+import Header from "./components/Header";
 
 class App extends Component {
   state = {
@@ -18,18 +18,9 @@ class App extends Component {
   render() {
     return(
         <body>
-          <header>
-            <Navbar/>
-            <div className="title-text-box">
-              <h1>Hackathon<br/>Spring 2019</h1>
-              <a className="btn btn-full" href="#">Register</a>
-              <a className="btn btn-ghost" href="#">Log in</a>
-            </div>
-
-          </header>
+          <Header/>
           <Guidelines/>
           <Teams/>
-
         </body>
     );
   }
