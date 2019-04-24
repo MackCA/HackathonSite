c9d921540a9a5bda019137f930da49d50f5b1477
Chloe Mack <38084397+UltionInvictus@users.noreply.github.com>
2019-04-23 21:39:46 -0400

    Update

--- functions/src/App.js
+++ functions/src/App.js
@@ -6,9 +6,13 @@ import './resources/vendors/fonts/_ionicons_svg_ios-bulb.svg';
 import './resources/vendors/fonts/_ionicons_svg_ios-mic.svg';
 import './resources/vendors/fonts/_ionicons_svg_ios-people.svg';
 import './resources/vendors/fonts/_ionicons_svg_ios-ribbon.svg';
+<<<<<<< HEAD
+=======
+import './resources/css/style.css'
+import Navbar from "./components/Navbar";
+>>>>>>> parent of 417e30f... Threw header into... well... Header.
 import Guidelines from "./components/Guidelines";
 import Teams from "./components/Teams";
-import Header from "./components/Header";
 
 class App extends Component {
   state = {
@@ -18,9 +22,18 @@ class App extends Component {
   render() {
     return(
         <body>
-          <Header/>
+          <header>
+            <Navbar/>
+            <div className="title-text-box">
+              <h1>Hackathon<br/>Spring 2019</h1>
+              <a className="btn btn-full" href="#">Register</a>
+              <a className="btn btn-ghost" href="#">Log in</a>
+            </div>
+
+          </header>
           <Guidelines/>
           <Teams/>
+
         </body>
     );
   }
